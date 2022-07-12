819f4864abd24a1d6ab2cc9869d936a1bb9e718c
康良玉 <kangly@vigamemedia.com>
2022-07-12 15:03:01 +0800

    自定义线程池添加暂停和恢复方法

---
+++ src/main/java/com/caogen/juc/threadpool/PauseableThreadPool.java
@@ -0,0 +1,98 @@
+package com.caogen.juc.threadpool;
+
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @Author 康良玉
+ * @Description 演示每个任务执行前后放钩子函数
+ * @Create 2022-07-12 14:52
+ */
+public class PauseableThreadPool extends ThreadPoolExecutor {
+
+    private final ReentrantLock lock = new ReentrantLock();
+    private Condition unpaused = lock.newCondition();
+    private boolean isPaused;
+
+    public PauseableThreadPool(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+    }
+
+    public PauseableThreadPool(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
+    }
+
+    public PauseableThreadPool(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
+    }
+
+    public PauseableThreadPool(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+    }
+
+    @Override
+    protected void beforeExecute(Thread t, Runnable r) {
+        super.beforeExecute(t, r);
+        lock.lock();
+        try {
+            while (isPaused) {
+                unpaused.await();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * 标记是否要暂停
+     */
+    private void pause() {
+        lock.lock();
+        try {
+            isPaused = true;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * 标记是否恢复
+     */
+    public void resume() {
+        lock.lock();
+        try {
+            isPaused = false;
+            unpaused.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        PauseableThreadPool pauseableThreadPool = new PauseableThreadPool(10, 20, 10l,
+                TimeUnit.SECONDS, new LinkedBlockingQueue());
+        Runnable runnable = new Runnable() {
+            public void run() {
+                System.out.println("我被执行");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        for (int i = 0; i < 10000; i++) {
+            pauseableThreadPool.execute(runnable);
+        }
+        Thread.sleep(1500);
+        pauseableThreadPool.pause();
+        System.out.println("线程池被暂停了");
+        Thread.sleep(1500);
+        pauseableThreadPool.resume();
+        System.out.println("线程池被恢复了");
+    }
+}
