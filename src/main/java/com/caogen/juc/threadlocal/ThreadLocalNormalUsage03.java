e4500449d8ad34f5e86d936e6cd6c631ea1ed535
康良玉 <kangly@vigamemedia.com>
2023-03-30 11:53:42 +0800

    java 线程池打印时间, ThreadLocal给每个线程分配自己的dateFormat对象

---
+++ src/main/java/com/caogen/juc/threadlocal/ThreadLocalNormalUsage03.java
@@ -0,0 +1,47 @@
+package com.caogen.juc.threadlocal;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @Author 康良玉
+ * @Description 线程池打印时间，利用ThreadLocal给每个线程分配自己的dateFormat对象，保证了线程安全，高效利用内容
+ * @Create 2023-03-30 10:51
+ */
+public class ThreadLocalNormalUsage03 {
+
+    public static ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 1000; i++) {
+            int finalI = i;
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    String date = new ThreadLocalNormalUsage03().date(finalI);
+                    System.out.println(date);
+                }
+            });
+        }
+        executorService.shutdown();
+    }
+
+    public String date(int seconds) {
+        Date date = new Date(1000 * seconds);
+        SimpleDateFormat dateFormat = ThreadSafeFormatter.dateFormatThreadLocal.get();
+        return dateFormat.format(date);
+    }
+
+}
+
+class ThreadSafeFormatter {
+    public static ThreadLocal<SimpleDateFormat> dateFormatThreadLocal = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        }
+    };
+}
+
