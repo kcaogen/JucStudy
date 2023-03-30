ded8a2098f329da9c6585f156d0ad7927d833f68
康良玉 <kangly@vigamemedia.com>
2023-03-30 12:29:45 +0800

    java ThreadLocal 当前线程内之间的数据共享，无需层层传递

---
+++ src/main/java/com/caogen/juc/threadlocal/ThreadLocalNormalUsage04.java
@@ -0,0 +1,52 @@
+package com.caogen.juc.threadlocal;
+
+/**
+ * @Author 康良玉
+ * @Description 演示ThreadLocal用法2: 避免传递参数的麻烦(无需层层传递参数，把用户信息在当前线程内所有方法共享)
+ * @Create 2023-03-30 12:19
+ */
+public class ThreadLocalNormalUsage04 {
+
+    public static void main(String[] args) {
+        new Service1().process();
+    }
+
+}
+
+
+class Service1 {
+
+    public void process() {
+        User user = new User("caogen");
+        UserContextHolder.holder.set(user);
+        new Service2().process();
+    }
+
+}
+
+class Service2 {
+    public void process() {
+        User user = UserContextHolder.holder.get();
+        System.out.println("service2: " + user.name);
+        new Service3().process();
+    }
+}
+
+class Service3 {
+    public void process() {
+        User user = UserContextHolder.holder.get();
+        System.out.println("service3: " + user.name);
+    }
+}
+
+class UserContextHolder {
+    public static ThreadLocal<User> holder = new ThreadLocal<>();
+}
+
+class User {
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
