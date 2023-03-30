64dd88902959e65c25165c983d1f70859f47d508
康良玉 <kangly@vigamemedia.com>
2023-03-30 17:20:27 +0800

    Lock锁 tryLock避免死锁发生

---
+++ src/main/java/com/caogen/juc/lock/lock/TryLockDeadLock.java
@@ -0,0 +1,94 @@
+package com.caogen.juc.lock.lock;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @Author 康良玉
+ * @Description 用tryLock来避免死锁
+ * @Create 2023-03-30 17:01
+ */
+public class TryLockDeadLock implements Runnable {
+
+    int flag = 1;
+    static Lock lock1 = new ReentrantLock();
+    static Lock lock2 = new ReentrantLock();
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            if (flag == 1) {
+                try {
+                    if (lock1.tryLock(800, TimeUnit.MILLISECONDS)) {
+                        try {
+                            System.out.println("线程1获取到了锁1");
+                            Thread.sleep(new Random().nextInt(1000));
+
+                            if (lock2.tryLock(800, TimeUnit.MILLISECONDS)) {
+                                try {
+                                    System.out.println("线程1获取到了锁2");
+                                    System.out.println("线程1成功获取到了两把锁");
+                                    break;
+                                } finally {
+                                    lock2.unlock();
+                                }
+                            } else {
+                                System.out.println("线程1获取锁2失败，已重试");
+                            }
+
+                        } finally {
+                            lock1.unlock();
+                            Thread.sleep(new Random().nextInt(1000));
+                        }
+                    } else {
+                        System.out.println("线程1获取锁1失败，已重试");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    if (lock2.tryLock(3000, TimeUnit.MILLISECONDS)) {
+                        try {
+                            System.out.println("线程2获取到了锁2");
+                            Thread.sleep(new Random().nextInt(1000));
+
+                            if (lock1.tryLock(800, TimeUnit.MILLISECONDS)) {
+                                try {
+                                    System.out.println("线程2获取到了锁1");
+                                    System.out.println("线程2成功获取到了两把锁");
+                                    break;
+                                } finally {
+                                    lock1.unlock();
+                                }
+                            } else {
+                                System.out.println("线程2获取锁1失败，已重试");
+                            }
+
+                        } finally {
+                            lock2.unlock();
+                            Thread.sleep(new Random().nextInt(1000));
+                        }
+                    } else {
+                        System.out.println("线程2获取锁2失败，已重试");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TryLockDeadLock t1 = new TryLockDeadLock();
+        TryLockDeadLock t2 = new TryLockDeadLock();
+
+        t1.flag = 1;
+        t2.flag = 0;
+
+        new Thread(t1).start();
+        new Thread(t2).start();
+    }
+}
